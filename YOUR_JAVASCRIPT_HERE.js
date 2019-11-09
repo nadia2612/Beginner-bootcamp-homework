a4e6b3001c362500932e971563d47cbced806571
nadia2612 <YOUR@EMAIL.com>
2019-11-09 13:25:27 +0100

    Updating hero stats

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -11,6 +11,23 @@ const hero = {
   }
 };
 
+// display stats
+
+const displayStats = person => {
+  const nameValue = document.getElementById("1");
+  nameValue.innerHTML = person.name;
+  //console.log(person.newName)
+  const healthValue = document.getElementById("2");
+  healthValue.innerHTML = person.health;
+
+  const weaponTypeValue = document.getElementById("3");
+  weaponTypeValue.innerHTML = person.weapon.type;
+
+  const weapoonDamegeValue = document.getElementById("4");
+  weapoonDamegeValue.innerHTML = person.weapon.damage;
+};
+displayStats(hero);
+
 const rest = person => {
   if (person.health == 10) {
     alert(`Your health is already ${person.health}`);
@@ -18,6 +35,7 @@ const rest = person => {
     person.health = 10;
   }
   return person;
+
 };
 
 const pickUpItem = (person, weapon) => {
@@ -29,3 +47,16 @@ const equipWeapon = person => {
     person.weapon = person.inventory[0];
   }
 };
+
+// changing the name of a hero
+
+const heroName = person => {
+  let newName = prompt(`Please incert a name of your hero`);
+  return (person.name = newName);
+};
+// heroName(hero);
+// console.log(hero.name);
+
+setTimeout(heroName(hero), 10000);
+
+displayStats(hero);
