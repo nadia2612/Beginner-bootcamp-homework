e0a04336b9c81568727858386e32c1e7f95f7a22
nadia2612 <YOUR@EMAIL.com>
2019-11-09 17:50:56 +0100

    CSS

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -14,28 +14,30 @@ const hero = {
 // display stats
 
 const displayStats = person => {
-  const nameValue = document.getElementById("1");
+  const nameValue = document.getElementById("t1");
   nameValue.innerHTML = person.name;
   //console.log(person.newName)
-  const healthValue = document.getElementById("2");
+  const healthValue = document.getElementById("t2");
   healthValue.innerHTML = person.health;
 
-  const weaponTypeValue = document.getElementById("3");
-  weaponTypeValue.innerHTML = person.weapon.type;
-
-  const weapoonDamegeValue = document.getElementById("4");
-  weapoonDamegeValue.innerHTML = person.weapon.damage;
+  if (person.weapon) {
+    const weaponTypeValue = document.getElementById("t3");
+    weaponTypeValue.innerHTML = person.weapon.type;
+    console.log(person.weapon.type);
+    const weapoonDamegeValue = document.getElementById("t4");
+    weapoonDamegeValue.innerHTML = person.weapon.damage;
+    console.log(person.weapon.damage);
+  }
 };
-displayStats(hero);
 
 const rest = person => {
   if (person.health == 10) {
     alert(`Your health is already ${person.health}`);
   } else {
     person.health = 10;
+    displayStats(person);
   }
   return person;
-
 };
 
 const pickUpItem = (person, weapon) => {
@@ -45,18 +47,20 @@ const pickUpItem = (person, weapon) => {
 const equipWeapon = person => {
   if (person.inventory.length > 0) {
     person.weapon = person.inventory[0];
+    displayStats(person);
   }
 };
 
 // changing the name of a hero
 
 const heroName = person => {
-  let newName = prompt(`Please incert a name of your hero`);
-  return (person.name = newName);
+  const newName = document.getElementById("name").value;
+  person.name = newName;
+  displayStats(person);
 };
-// heroName(hero);
-// console.log(hero.name);
-
-setTimeout(heroName(hero), 10000);
 
 displayStats(hero);
+
+//setTimeout(heroName(hero), 10000);
+
+//displayStats(hero);
